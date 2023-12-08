d53989321739e1073e5d77405fddb3c299b3d66d
Astralzz <daiinxd@gmail.com>
2023-12-07 21:36:28 -0600

    ver 2.0.1: Nuevos comandos artisan de limpiar

--- app/routes/console.php
+++ app/routes/console.php
@@ -72,3 +72,52 @@ Artisan::command('insert-cursos-random', function () {
         $this->error('Ocurrió un error: ' . $e->getMessage());
     }
 })->describe('Inserta 50 usuarios aleatorios a la tabla cursos.');
+
+
+// * php artisan clear-env-app
+Artisan::command('clear-env-app', function () {
+    try {
+
+        // Ejecutamos composer dump-autoload
+        exec('composer dump-autoload');
+
+        // Limpiamos
+        $this->call('cache:clear');
+        $this->call('config:clear');
+        $this->call('view:clear');
+        $this->info('Caché limpiada con éxito.');
+
+        // ! Error
+    } catch (\Throwable $th) {
+        $this->error('No se limpio el env correctamente, ' . $th->getMessage());
+    }
+})->purpose('Limpiar la caché del .env de la aplicación');
+
+// * php artisan clear-cache-app
+Artisan::command('clear-cache-app', function () {
+    try {
+
+        // Limpiamos
+        $this->call('clear-compiled');
+        $this->call('cache:clear');
+        $this->call('config:cache');
+        $this->call('config:clear');
+        $this->call('route:clear');
+        $this->call('route:cache');
+        $this->call('view:clear');
+        $this->info('Caché limpiada con éxito.');
+
+        // ! Error
+    } catch (\Throwable $th) {
+        $this->error('No se limpio la app correctamente, ' . $th->getMessage());
+    }
+})->purpose('Limpiar la caché de la aplicación');
+
+// * php artisan optimize-app
+Artisan::command('optimize-app', function () {
+    $this->call('clear-compiled');
+    $this->call('optimize:clear');
+    $this->call('config:cache');
+    $this->call('route:cache');
+    $this->info('Optimización completada.');
+})->purpose('Optimizar la aplicación para un mejor rendimiento');
